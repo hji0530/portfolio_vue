06f64111d402362070ab1eca5b53bf7032357255
[khjee] <[forget019@gmail.com]>
2025-04-13 22:34:35 +0900

    khj

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,15 +1,5 @@
 <template>
   <div>
-    <section class="home">
-      <div class="content">
-        <h1>포트폴리오</h1>
-
-        <div class="buttons">
-          <router-link to="/sub1" class="btn">서브페이지 1</router-link>
-          <router-link to="/sub2" class="btn">서브페이지 2</router-link>
-        </div>
-      </div>
-    </section>
     <!-- INTRO -->
     <section class="section-01">
       <div class="l-center">
